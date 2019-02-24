87719aa043f69104fe4c6305273bb95d31481e28
minikiller@qq.com <minikiller@qq.com>
2019-02-25 07:24:33 +0800

    add camel test

---
+++ pax-keycloak-camel/src/main/java/org/ops4j/pax/keycloak/camel/CamelHelloProcessor.java
@@ -0,0 +1,24 @@
+package org.ops4j.pax.keycloak.camel;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.representations.AccessToken;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
+ */
+public class CamelHelloProcessor implements Processor {
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        HttpServletRequest req = exchange.getIn().getBody(HttpServletRequest.class);
+        KeycloakPrincipal keycloakPrincipal = (KeycloakPrincipal) req.getUserPrincipal();
+        AccessToken accessToken = keycloakPrincipal.getKeycloakSecurityContext().getToken();
+        String username = accessToken.getPreferredUsername();
+        String fullName = accessToken.getName();
+
+        exchange.getOut().setBody("Hello " + username + "! Your full name is " + fullName + ".");
+    }
+}
